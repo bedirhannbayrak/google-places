0d2ea98264cac8b0c560e69803e2826df01146d3
bedirhannbayrak <bedirhan.bayrak41@gmail.com>
2021-10-20 18:15:17 +0300

    first commit

---
+++ src/test/java/com/bedirhan/google/places/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bedirhan.google.places.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
